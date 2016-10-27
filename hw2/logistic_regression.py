c845473f35496120e10d76605e01acc3777fb905
topjohnwu <topjohnwu@gmail.com>
2016-10-27 22:58:51 +0800

    HW2: Slight improvement

--- hw2/logistic_regression.py
+++ hw2/logistic_regression.py
@@ -5,22 +5,25 @@ from operator import mul
 train_data = []
 train_ans = []
 
-params = [0 for x in range(58)]
-grad = [0 for x in range(58)]
-grad_sq_sum = [0 for x in range(58)]
+params = [0.0 for x in range(58)]
+grad = [0.0 for x in range(58)]
+grad_sq_sum = [0.0 for x in range(58)]
 
 loop = 100
 l_rate = 0.001
 
 def estim(data):
-	num = 0
+	num = 0.0
 	for i in range(len(data)):
 		num += sum(map(mul, data, params[:len(data)]))
 	num += params[len(params) - 1]
 	return num
 
 def sigmoid(num):
-	return 1 / (1 + math.exp(0 - num))
+	try:
+		return 1 / (1 + math.exp(0 - num))
+	except OverflowError:
+		return 0.0
 	
 def rate(i):
 	return l_rate / math.sqrt(1 if grad_sq_sum[i] == 0 else grad_sq_sum[i])
@@ -36,7 +39,7 @@ def updateParam():
 	for i in range(len(params)):
 		grad_sq_sum[i] += math.pow(grad[i], 2)
 		params[i] -= rate(i) * grad[i]
-	grad = [0 for x in range(58)]
+	grad = [0.0 for x in range(58)]
 
 def predict():
 	num = 0.0
